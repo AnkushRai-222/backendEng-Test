6f0b7f2c2590a53dc17dc59b788156342e1c4aa8
AnkushRai-222 <ankushrai222@gmail.com>
2023-03-20 10:53:48 +0530

    done

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import inquirer from 'inquirer';
+import chalk from 'chalk'
 
-const events = [
-  { text: 'textOne', dateTime: '2020-07-10 15:00:00.000' },
-  { text: 'textTwo', dateTime: '2020-07-11 16:00:00.000' },
-  { text: 'textThree', dateTime: '2020-07-12 17:00:00.000' },
-  { text: 'textFour', dateTime: '2020-07-13 18:00:00.000' },
-  { text: 'textFive', dateTime: '2020-07-14 19:00:00.000' },
-  { text: 'textSix', dateTime: '2020-07-15 20:00:00.000' },
-  { text: 'textSeven', dateTime: '2020-07-16 21:00:00.000' },
-  { text: 'textEight', dateTime: '2020-07-17 22:00:00.000' },
-  { text: 'textNine', dateTime: '2020-07-18 23:00:00.000' },
-  { text: 'textTen', dateTime: '2020-07-19 00:00:00.000' },
-];
+// Prompt the user for the number of players
+inquirer.prompt([
+  {
+    type: 'number',
+    name: 'numPlayers',
+    message: 'How many players are participating?',
+    validate: value => {
+      if (value <= 0) {
+        return 'Please enter a positive number';
+      }
+      return true;
+    }
+  }
+]).then(({ numPlayers }) => {
+  // Generate the fixture
+  const rounds = [];
+  const halfNumPlayers = Math.floor(numPlayers / 2);
+  for (let round = 0; round < numPlayers - 1; round++) {
+    const matches = [];
+    for (let i = 0; i < halfNumPlayers; i++) {
+      const home = (round + i) % (numPlayers - 1);
+      const away = (numPlayers - 1 - i + round) % (numPlayers - 1);
+      matches.push(`${home + 1} vs ${away + 1}`);
+    }
+    rounds.push(matches);
+  }
 
-app.get('/schedule', (req, res) => {
-  events.forEach((event) => {
-    setTimeout(() => {
-      console.log(event.text.split('').reverse().join(''));
-    }, event.text.length * 1000);
+  // Print the fixture
+  console.log(chalk.green('Round Robin Tournament Fixture'));
+  rounds.forEach((matches, index) => {
+    console.log(chalk.blue(`Round ${index + 1}:`));
+    matches.forEach(match => {
+      console.log(match);
+    });
   });
-  res.send('Events have been scheduled');
-});
-
-app.listen(port, () => {
-  console.log(`Scheduler app listening at http://localhost:${port}`);
 });
